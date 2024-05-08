59546187cfd503cdafdf32e80c5a41d5b92d830b
Ēriks Olavs <eo00789@rvt.lv>
2024-05-08 05:28:33 +0300

    2 version

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,18 @@
-﻿using System.Windows;
+﻿using Calender.UserControls;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Calender
 {
     public partial class MainWindow : Window
     {
+
+        private string noteText = "";
+        private string timeText = "";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,16 +40,66 @@ namespace Calender
                 lblNote.Visibility = Visibility.Collapsed;
             else
                 lblNote.Visibility = Visibility.Visible;
-        }
 
-        private void txtTime_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+            noteText = txtNote.Text;
+
+            string NoteText = txtNote.Text;
+
+        us: Item targetItem = new Item();
+
+
+
+            targetItem.Title = NoteText;
+            targetItem.Time = txtTime.Text;
+
+
+
+        }
+        private void txtTime_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(txtTime.Text) && txtTime.Text.Length > 0)
-                lblTime.Visibility = Visibility.Collapsed;
-            else
-                lblTime.Visibility = Visibility.Visible;
+            TextBox textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                // Find the corresponding Item control based on the visual tree
+                Item item = FindParent<Item>(textBox);
+                if (item != null)
+                {
+                    // Update the Time property of the corresponding Item
+                    timeText = textBox.Text;
+                    item.Time = textBox.Text;
+                }
+            }
+        }
+        private T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            if (parentObject == null)
+                return null;
+
+            T parent = parentObject as T;
+            return parent ?? FindParent<T>(parentObject);
         }
 
+
+        private void CreateNewItem(object sender, RoutedEventArgs e)
+        {
+            // Create a new Item control
+            Item newItem = new Item();
+
+            // Set properties from noteText and timeText
+            newItem.Title = noteText;
+            newItem.Time = timeText;
+
+
+            // Clear txtNote and txtTime
+            txtNote.Text = "";
+            txtTime.Text = "";
+
+            // Clear noteText and timeText for future use
+            noteText = "";
+            timeText = "";
+        }
         private void Item_Loaded(object sender, RoutedEventArgs e)
         {
 
